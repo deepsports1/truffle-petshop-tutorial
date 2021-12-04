a5cc2e4faa5e2f9f9b7ae7cd5325aa72330a7b6e
Josh Reback <deepsports1610@gmail.com>
2021-12-04 15:55:18 -0500

    got pets to show up - next step is getting the Adoption.json and Migrations.json able to be served by IPFS

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -2,6 +2,15 @@ pragma solidity ^0.5.0;
 
 contract Adoption {
 	address[16] public adopters;
+	address public owner;
+
+	constructor() public {
+		owner = msg.sender;
+	}
+
+	function kill() public {
+		require(msg.sender == owner);		
+	}
 
 	function adopt(uint petId) public returns (uint) {
 		require(petId >= 0 && petId <= 15);
--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic = process.env.mnemonic;
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -9,6 +12,12 @@ module.exports = {
     },
     develop: {
       port: 8545
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/0f2fd6eb9b2e431d8842fb2641370a7f")
+      },
+      network_id: 4
     }
   }
 };
